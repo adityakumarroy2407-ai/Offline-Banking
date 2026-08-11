b72999a9b0bf5e4e49c319c377548569186c6cae
Aditya Roy <adityakumarroy2407@gmail.com>
2026-08-11 15:06:38 +0600

    update

--- main.py
+++ main.py
@@ -1,59 +1,76 @@
 import json
-print("1. SEND MONEY")
-print("2. cash out")
-print("3. Mobile recharge")
-print("4. Download Bkash app")
-print("5. My bkash")
-print("6. Exit")
 accountBalance = 140
+while True:
+    print("1. SEND MONEY")
+    print("2. cash out")
+    print("3. Mobile recharge")
+    print("4. Download Bkash app")
+    print("5. My bkash")
+    print("6. Exit")
+   
 
-n = int(input("Enter your choice: "))
-if(n>6):
-    print("Enter a number between 1 to 6")
 
-elif(n==1):
-    reciever=(input("Enter reciever bkash account number:"))
-    amount=int (input("Enter amount: "))
-    if(amount>accountBalance):
-        print("You donot have sufficiant balance")
-    else:
-        print("Succesfully send",amount,"tk")
-        accountBalance=accountBalance-amount
-        print ("New balance:",accountBalance)
-elif n==2:
-    print("1. From agent")
-    print("2. From ATM")
-    m=int(input("Enter your choice:"))
-    if m==1:
-        agent=input("Enter agent number:")
+    n = int(input("Enter your choice: "))
+    if(n>6):
+        print("Enter a number between 1 to 6")
+
+    elif(n==1):
+        reciever=(input("Enter reciever bkash account number:"))
+        amount=int (input("Enter amount: "))
+        if(amount>accountBalance):
+            print("You donot have sufficiant balance")
+        else:
+            print("Succesfully send",amount,"tk")
+            accountBalance=accountBalance-amount
+            print ("New balance:",accountBalance)
+    elif n==2:
+        print("1. From agent")
+        print("2. From ATM")
+        m=int(input("Enter your choice:"))
+        if m==1:
+            agent=input("Enter agent number:")
+            amount=int(input("Enter amount:"))
+            if(amount>accountBalance):
+                print("You donot have sufficiant balance")
+            else:
+                print("Succesefully cashout",amount,"tk")
+                accountBalance=accountBalance-(amount*0.0139+amount)
+                print("Your new balance is",accountBalance)
+        else:
+            pin=int(input("Enter your pin"))
+            amount=int(input("Enter amount"))
+            if(amount>accountBalance):
+                print("You donot have sufficiant balance")
+            else:
+                print("Succesefully cashout",amount)
+                accountBalance=accountBalance-(amount*0.01+amount)
+                print("Your new balance is",accountBalance)
+
+    elif n==3:
+        MobileNo=input("Enter mobile number:")
         amount=int(input("Enter amount:"))
-        if(amount>accountBalance):
+        if amount>accountBalance:
             print("You donot have sufficiant balance")
         else:
-            print("Succesefully cashout",amount,"tk")
-            accountBalance=accountBalance-(amount*0.0139+amount)
+            accountBalance=accountBalance-amount
+            print("you have successfully recharged with amount",amount,"BDT")
             print("Your new balance is",accountBalance)
-    else:
-        pin=int(input("Enter your pin"))
-        amount=int(input("Enter amount"))
-        if(amount>accountBalance):
-            print("You donot have sufficiant balance")
+
+    elif n==4:
+        print("1. for android")
+        print("2. for ios")
+        choice=int(input("Enter your choice"))
+        if choice==1:
+            print("Go to play store and search Bkash")
         else:
-            print("Succesefully cashout",amount)
-            accountBalance=accountBalance-(amount*0.01+amount)
-            print("Your new balance is",accountBalance)
+            print("Go to app store and search Bkash")
 
-elif n==3:
-    MobileNo=input("Enter mobile number:")
-    amount=int(input("Enter amount:"))
-    if amount>accountBalance:
-         print("You donot have sufficiant balance")
-    else:
-        accountBalance=accountBalance-amount
-        print("you have successfully recharged with amount",amount,"BDT")
-        print("Your new balance is",accountBalance)
-
-
-
-    
-
+    elif n==5:
+        print("1. check balance")
+        print("2. add priyo number")
+        choice=int (input("Enter your choice"))
+        if choice==1:
+            print("your account balance is ",accountBalance)
+        else :
+            print("add 2 priyo number")
+            
